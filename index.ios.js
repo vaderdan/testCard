c751b8aa7237bce9320fc86749f7bcec1fe2e638
Danny Lazarow <danny.lazarow@gmail.com>
2017-02-27 11:52:07 +0200

    remove flexDirection style

--- index.ios.js
+++ index.ios.js
@@ -46,7 +46,7 @@ export default class testCard extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
+    // flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
